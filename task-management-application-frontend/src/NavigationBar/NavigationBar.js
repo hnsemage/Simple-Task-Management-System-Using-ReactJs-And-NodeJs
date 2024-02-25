c9793f6fa40ee6aa212228673fc459675a0adc5e
hnsemage <hnsemage@students.nsbm.ac.lk>
2024-02-25 23:31:45 +0530

    Update in Navigation bar, remove profile icon

--- task-management-application-frontend/src/NavigationBar/NavigationBar.js
+++ task-management-application-frontend/src/NavigationBar/NavigationBar.js
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import { Link } from 'react-router-dom';
-import { Home as HomeIcon, Info as InfoIcon, AccountCircle as ProfileIcon } from '@mui/icons-material';
+import { Home as HomeIcon } from '@mui/icons-material';
 
 const NavigationBar = () => {
   return (
@@ -15,7 +15,6 @@ const NavigationBar = () => {
           
         </Typography>
         <IconButton color="inherit" component={Link} to="/"><HomeIcon style={{ fontSize: 30 }}/></IconButton>
-        <IconButton color="inherit" component={Link} to="/register"><ProfileIcon style={{ fontSize: 30 }}/></IconButton>
       </Toolbar>
     </AppBar>
   );
